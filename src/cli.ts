3a8ed2f9d45ac8338528cfefe69b385b66c85b2d
lonewolfyx <877085893@qq.com>
2026-03-03 21:02:56 +0800

    feat(cli): enhance CLI with developer profile and tech stack display

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,60 @@
-import { intro, outro } from '@clack/prompts'
+import { intro, log, note, outro } from '@clack/prompts'
 import { createMain, defineCommand } from 'citty'
 import { description, name, version } from 'package.json'
+
+interface IStack {
+    label: string
+    stacks: string[]
+}
+
+const stack: IStack[] = [
+    {
+        label: 'Frontend',
+        stacks: [
+            'JavaScript',
+            'TypeScript',
+            'Vue.js',
+            'React',
+            'Nuxt',
+            'TailwindCSS',
+        ],
+    },
+    {
+        label: 'Backend',
+        stacks: [
+            'Node.js',
+            'NestJS',
+            'Python',
+            'PHP',
+        ],
+    },
+    {
+        label: 'Runtime',
+        stacks: [
+            'Bun',
+            'Pnpm',
+            'Node',
+        ],
+    },
+    {
+        label: 'Tooling',
+        stacks: [
+            'Vite',
+            'Rolldown',
+            'Tsup',
+            'TsDown',
+            'Turbo',
+        ],
+    },
+    {
+        label: 'Database',
+        stacks: [
+            'PostgreSQL',
+            'MySQL',
+            'Redis',
+        ],
+    },
+]
 
 const command = defineCommand({
     meta: {
@@ -14,8 +68,25 @@ const command = defineCommand({
     cleanup() {
         outro('welcome review my info')
     },
-    run({ args }) {
-        console.log(args)
+    run() {
+        note(
+            [
+                'lonewolfyx',
+                'Design Engineer',
+                'Open Source Contributor',
+            ].join(' · '),
+            '$ whoami',
+        )
+
+        note(
+            stack.map((s) => {
+                return `> ${s.label.padEnd(10)} :: ${s.stacks.join(', ')}`
+            }).join('\n'),
+            '$ echo stack',
+        )
+
+        log.success('$ echo github\n https://github.com/lonewolfyx')
+        log.success('$ echo website\n https://lonewolfyx.vercel.app')
     },
 })
 
